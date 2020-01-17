7c70a01e5e7311d889f3c2dff99d1d3eb127cf87
dani <dani.zaghian@gmail.com>
2020-01-17 10:53:10 -0800

    adds query params and url segment params

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -5,4 +5,32 @@ class Api::ParamsExamplesController < ApplicationController
     render "query_params.json.jb"
   end
 
+  def name_action
+    @name = params["name"].upcase
+    @message = @name
+    if @name.starts_with?("A")
+      @message = "Hey your name starts with A!"
+    end
+    render "name.json.jb"
+  end
+
+  def guess_query_action
+    user_guess = params["guess"].to_i
+    winning_number = 42
+    if user_guess > winning_number
+      @message = "Too high!"
+    elsif user_guess < winning_number
+      @message = "Too low!"
+    else
+      @message = "You win!"
+    end
+    render "guess_query.json.jb"
+  end
+
+  def segment_params_action
+    my_value = params["purple_hippo"]
+    @message = "The url segment is #{my_value}"
+    render "segment_params.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/query_params_url" => "params_examples#query_params_action"
+    get "/name_url" => "params_examples#name_action"
+    get "/guess_query_url" => "params_examples#guess_query_action"
+
+    get "/segment_params_url/:purple_hippo" => "params_examples#segment_params_action"
   end
 end
+
